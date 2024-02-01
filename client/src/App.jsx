70a41b24ff78b114be81916c96649ba238b5fb8c
amine <amine001op@gmail.com>
2024-02-01 22:36:40 +0000

    Client: add footer content, update css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,16 @@ import Header from "./components/Header"
 function App() {
 
   return (
-    <div className="App">
+    <div className="App container">
+
       <Header />
+      
       <main className="main">
         {/* dynamic main content */}
       </main>
+      
       <Footer />
+      
     </div>
   )
 }
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,13 +1,56 @@
 import "./Footer.css"
+import app_ios from "../assets/app_ios.svg"
+import app_android from "../assets/app_android.png"
+import footer_fb from "../assets/footer_fb.svg"
+import footer_in from "../assets/footer_in.svg"
+import footer_li from "../assets/footer_li.svg"
+import footer_tw from "../assets/footer_tw.svg"
 
 const Footer = () => {
   return (
     <footer className="footer">
+      
+      <div className="footer-grid">
+        
+        <div className="flex-col gap-0-5 p-0-5">
+          <a href="#">About us</a>
+          <a href="#">Terms</a>
+          <a href="#">Privacy</a>
+          <a href="#">Help</a>
+        </div>
+
+        <div className="p-0-5">
+          <div className="h5 mb-1">FOLLOW US</div>
+
+          <div className="flex-row gap-0-5 wrap">
+            <a href="#" className="social-link">
+              <img src={footer_fb} alt="facebook" />
+            </a>
+            <a href="#" className="social-link">
+              <img src={footer_in} alt="instagram" />
+            </a>
+            <a href="#" className="social-link">
+              <img src={footer_li} alt="linkedin" />
+            </a>
+            <a href="#" className="social-link">
+              <img src={footer_tw} alt="twitter" />
+            </a>
+          </div>
+        </div>
+
+        <div className="flex-col gap-1 p-0-5">
+          <a href="#"> <img src={app_ios} alt="app_ios" /> </a>
+          <a href="#"> <img src={app_android} alt="app_android" /> </a>
+        </div>
+      
+      </div>
+
       <div className="copyright">
         <span>&copy; Copyright 2024</span>
         {" "}
         <span><a>iBook-Dock</a></span>
       </div>
+
     </footer>
   )
 }
