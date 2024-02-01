e8b6d25c42dedc3440e27e03961e44df71d6229d
amine <amine001op@gmail.com>
2024-02-01 02:04:09 +0000

    Server: Add custum logging for incoming requests and generated errors

---
+++ server/server.js
@@ -0,0 +1,78 @@
+/*-----
+imports
+-------*/
+
+import express from "express";
+import path from "path"
+import { fileURLToPath } from "url"
+import dotenv from "dotenv"
+import cors from "cors"
+import mongoose from "mongoose";
+import helmet from "helmet";
+
+import { requestLogger } from "./middlewares/requestLogger.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
+
+
+/*------------
+configurations
+--------------*/
+
+// Creates an Express application
+const app = express();
+
+// Load .env file contents into process.env 
+dotenv.config()
+
+const __filename = fileURLToPath(import.meta.url)
+// note: __dirname related to server.js
+export const __dirname = path.dirname(__filename)
+
+
+/*---------
+middlewares
+-----------*/
+
+// set security HTTP headers
+app.use(helmet());
+
+// parse json and urlencoded in request body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// enable cors
+app.use(cors());
+
+// reqLogger
+app.use(requestLogger)
+
+
+/*------------
+Route Handlers
+--------------*/
+
+app.get("/", (req, res) => {
+  res.send("connected");
+});
+
+// custom 404
+app.use('*', (req, res) => {
+  res.status(404).send('Sorry, page not found')
+})
+
+// errLogger
+app.use(errorHandler)
+
+
+/*--------------------
+Listen for connections
+----------------------*/
+
+const port = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(conn => {
+    console.log(`connected to ${conn.connection.name} db.`)
+    app.listen(port, () => console.log(`Server listening on port ${port}.`));
+  })
+  .catch(err => console.log(err))
