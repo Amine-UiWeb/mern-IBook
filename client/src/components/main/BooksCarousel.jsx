67e650b4fa2c5a0b68702894ccf4d65e9d08e9e2
amine <amine001op@gmail.com>
2024-02-05 23:02:49 +0000

    Client: minor changes in BooksCarousel component

--- client/src/components/main/BooksCarousel.jsx
+++ client/src/components/main/BooksCarousel.jsx
@@ -1,17 +1,17 @@
+import CarouselCard from "./CarouselCard"
 import "./BooksCarousel.css"
 
 import data from "../../assets/db/data.json"
-import CarouselCard from "./CarouselCard"
 
 const BooksCarousel = () => {
 
   return (      
     <div className="carousel-container">
       
-      {/* display books with placeholer data, 3 books in each genre */}
+      {/* display books with placeholer data (3 books in each genre) */}
 
       { 
-        data.data.length && data.data.map((book, i) => (
+        data?.books?.length && data.books.map((book, i) => (
             <CarouselCard key={i} book={book} />
           )
         )
