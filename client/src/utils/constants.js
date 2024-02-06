a4768c99f17ec53a50ade35542132ca65c30311d
amine <amine001op@gmail.com>
2024-02-06 16:36:29 +0000

    Client: Editing contants.js

---
+++ client/src/utils/constants.js
@@ -0,0 +1,8 @@
+export const urls = {
+  Literature: 'https://openlibrary.org/subjects/Literature.json',
+  Romance: 'https://openlibrary.org/subjects/Romance.json',
+  Comedy: 'https://openlibrary.org/subjects/Comedy.json',
+  Horror: 'https://openlibrary.org/subjects/Horror.json',
+  Thriller: 'https://openlibrary.org/subjects/Thriller.json',
+  Detective: 'https://openlibrary.org/subjects/Detective.json',
+}
