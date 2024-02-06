44ce8c63ae5533f2eefd8966f1e034fe09047086
amine <amine001op@gmail.com>
2024-02-06 19:55:48 +0000

    Client: Modifying LandingPage to fetch books for all specified subjects

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,49 +1,55 @@
+import { useState, useEffect } from "react"
+
+import { urls } from "../utils/constants"
+
 import BooksCarousel from "../components/main/BooksCarousel"
 import "./LandingPage.css"
 
+
 const LandingPage = () => {
+
+  const [books, setBooks] = useState({})
+
+  useEffect(() => {
+
+    const fetchBooks = async (url) => {
+      fetch(url, { cache: 'force-cache' })
+        .then(res => res.json())
+        .then(data => {
+          // append the fetched books to the state 
+          setBooks(prev => ({ ...prev, [data.name]: data.works }))
+        })
+    }
+    
+    // fetch books for each specified subject 
+    Object.keys(urls).forEach(subject => {
+      fetchBooks(urls[subject])
+    })
+
+  }, [])
+
+  // console.log(books)
 
   return (
     <div className="landing-page">
+      {
+        // pass each subject books to a BooksCarousel componenet
+        Object.keys(books).map(book => (
+          <section key={book} className="books-section">
+            
+            <h3 className="h3 ff-times p-1">
+              <a href="#" className="hover-underline">
+                {book.toString().toUpperCase()}
+              </a>
+            </h3>
 
-      <section className="books-section">
-        <h3 className="h3 ff-times p-1">
-          <a href="#" className="hover-underline">Literature</a>
-        </h3>
-      
-        <div className="list-container">
-          <BooksCarousel />
-        </div>
+            <div className="books-container">
+              <BooksCarousel books={books[book]} />
+            </div>
 
-      </section>
-
-      <section className="books-section">
-        <h3 className="h3 ff-times p-1">
-          <a href="#" className="hover-underline">Comedy</a>
-        </h3>
-        <div className="list-container">
-          <BooksCarousel />
-        </div>
-      </section>
-
-      <section className="books-section">
-        <h3 className="h3 ff-times p-1">
-          <a href="#" className="hover-underline">Thriller</a>
-        </h3>
-        <div className="list-container">
-          <BooksCarousel />
-        </div>
-      </section>
-
-      <section className="books-section">
-        <h3 className="h3 ff-times p-1">
-          <a href="#" className="hover-underline">Horror</a>
-        </h3>
-        <div className="books-container">
-          <BooksCarousel />
-        </div>
-      </section>
-      
+          </section>
+        ))
+      }  
     </div>
   )
 }
