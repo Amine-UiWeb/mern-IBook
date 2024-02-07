8cd10fa558e391f86b599516a7ac01c8ec19ec1b
amine <amine001op@gmail.com>
2024-02-07 23:55:30 +0000

    Client: small change to BooksCarousel component

--- client/src/components/main/BooksCarousel.jsx
+++ client/src/components/main/BooksCarousel.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react"
 import CarouselCard from "./CarouselCard"
 import "./BooksCarousel.css"
 
@@ -8,12 +7,13 @@ const BooksCarousel = ({ books }) => {
 
   return (      
     <div className="carousel-container">
-      
+      <button>{"\u2770"}</button>
       {
-        books?.length > 0 && 
-          books.map((book, i) => <CarouselCard key={i} book={book} />)
+        books?.length > 0 && books.map(
+            (book, i) => <CarouselCard key={i} book={book} />
+          )
       }
-
+      <button>{"\u2771"}</button>
     </div>
   )
 }
