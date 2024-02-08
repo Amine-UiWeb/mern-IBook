e5c3ececd21474ed0b6702ae5fc49e528f076c65
amine <amine001op@gmail.com>
2024-02-08 18:01:46 +0000

    Client: implement image caching manually

--- client/src/components/main/CarouselCard.jsx
+++ client/src/components/main/CarouselCard.jsx
@@ -1,13 +1,30 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
 import "./CarouselCard.css"
 
-const CarouselCard = (props) => {
+const CarouselCard = ({ book }) => {
+
+  // i'm requesting image data as blob data to force caching the images
+  // can't cache the images by just setting the url in the src 
+
+  const [image, setImage] = useState(null)
+  const url = `https://covers.openlibrary.org/b/id/${book?.cover_id}-L.jpg`
+
+  useEffect(() => {
+    (async () => {
+      const res = await fetch(url, { cache: 'force-cache' })
+      const blob = await res.blob()
+      
+      let reader = new FileReader()
+      reader.onload = function() { setImage(this.result) }
+      reader.readAsDataURL(blob)
+    })()
+  }, [])
 
   return ( 
     <div className="carousel-card">
       <a href="#">
-        <img src={
-          `https://covers.openlibrary.org/b/id/${props.book?.cover_id}-L.jpg`
-        }/>
+        <img src={image}/>
       </a>
     </div>
   )
