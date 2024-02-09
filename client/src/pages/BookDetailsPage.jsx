cdc28466f1096a9fbaa1f67a0706a88a4d0771ae
amine <amine001op@gmail.com>
2024-02-09 16:49:11 +0000

    Client: set basic structure for BookDetailsPage component

--- client/src/pages/BookDetailsPage.jsx
+++ client/src/pages/BookDetailsPage.jsx
@@ -2,7 +2,14 @@ import "./BookDetailsPage.css"
 
 const BookDetailsPage = () => {
   return (
-    <div>BookDetailsPage</div>
+    <div className="bookDetails">
+      <h3 className="h3">The history of everything</h3>
+      <br />
+      <img src="" alt="book-cover_id" className="mbo-1" />
+      <h5 className="h5 mbo-1">Book Details:</h5>
+      <h5 className="h5 mbo-1">Description:</h5>
+      <h5 className="h5 mbo-1">Author:</h5>
+    </div>
   )
 }
 export default BookDetailsPage
