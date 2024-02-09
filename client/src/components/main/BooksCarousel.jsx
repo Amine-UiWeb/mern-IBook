26d4e33ec0992154c9c7e6a78c35756d9b7fb9cc
amine <amine001op@gmail.com>
2024-02-09 02:22:51 +0000

    Client: implement scrolling to BooksCarousel component using useRef and styles in BooksCarousel.css

--- client/src/components/main/BooksCarousel.jsx
+++ client/src/components/main/BooksCarousel.jsx
@@ -1,19 +1,37 @@
+import { useRef } from "react"
 import CarouselCard from "./CarouselCard"
 import "./BooksCarousel.css"
 
 const BooksCarousel = ({ books }) => {
 
+  const myRef = useRef()
+
+  const handleScroll = (direction) => {
+    if (direction === 'left') {
+      myRef.current.scrollLeft -= 170
+    }
+    else {
+      myRef.current.scrollLeft += 170
+    }
+  }
+
   console.log(books)
 
   return (      
     <div className="carousel-container">
-      <button>{"\u2770"}</button>
-      {
-        books?.length > 0 && books.map(
-            (book, i) => <CarouselCard key={i} book={book} />
-          )
-      }
-      <button>{"\u2771"}</button>
+
+      <button onClick={() => handleScroll('left')}>{"\u2770"}</button>
+      
+      <div className="carousel-wrapper" ref={myRef}>
+        {
+          books?.length > 0 && books.map(
+              (book, i) => <CarouselCard key={i} book={book} />
+            )
+        }
+      </div>
+      
+      <button onClick={() => handleScroll('right')}>{"\u2771"}</button>
+    
     </div>
   )
 }
