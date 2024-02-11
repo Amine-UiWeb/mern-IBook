da44a1551ffe1c4ac7c52a9f028f89377ad1142d
amine <amine001op@gmail.com>
2024-02-11 20:54:34 +0000

    Client: small adjustements for BookDetailsPage

--- client/src/pages/BookDetailsPage.jsx
+++ client/src/pages/BookDetailsPage.jsx
@@ -1,10 +1,10 @@
-import { useEffect } from "react"
 import { Link, useLocation } from "react-router-dom"
 
 import useFetchImage from "../utils/hooks/useFetchImage.js"
 import useFetchData from "../utils/hooks/useFetchData.js"
 
 import "./BookDetailsPage.css"
+import { useState } from "react"
 
 
 const BookDetailsPage = () => {
@@ -12,8 +12,9 @@ const BookDetailsPage = () => {
   // accessing the state data passed to the navigated location  
   const location = useLocation()
   const data = location.state
-  // console.log(data)
-    
+  console.log(data)
+  
+  // destructuring
   const {
     title, 
     cover_id, 
@@ -24,29 +25,40 @@ const BookDetailsPage = () => {
     availability, key
   } = data
 
-  // extract informations
-  const author = authors?.[0]?.name
+  // extract information
+  const authorName = authors?.[0]?.name
+  const authorKey = authors?.[0]?.key
   const olWork = availability?.openlibrary_work || key?.split('/works/')[1] 
   const oledition = availability?.openlibrary_edition
   // console.log("olWork: ", olWork, "oledition: ", oledition)
-  
-  
+    
 
   // fetch book cover
   const coverUrl = `https://covers.openlibrary.org/b/id/${cover_id}-L.jpg`
   const { image } = useFetchImage(coverUrl)
 
 
-  // fetch work
+  // fetch work info
   const workUrl = `https://openlibrary.org/works/${olWork}.json`
   const { data: workData } = useFetchData(workUrl)
-  const description = workData?.description?.value ?? workData?.description
-  console.log(workData)
-  
-  // fetch rating
+  const description = workData?.description?.value || workData?.description
+
+  // fetch rating data
   const ratingUrl = `https://openlibrary.org/works/${olWork}/ratings.json`
   const { data: ratings } = useFetchData(ratingUrl)
   const rating = ratings?.summary?.average?.toFixed(2)
+  
+  
+  // // fetch author info
+  // const { data: authorInfo } = 
+  //   useFetchData(`https://openlibrary.org/authors/${author}.json`)
+  // console.log(authorInfo)
+  // const {  } = authorInfo
+
+  // // fetch author works
+  // const { data: authorWorks } = 
+  //   useFetchData(`https://openlibrary.org/authors/OL23919A/works.json`)
+  // console.log(authorWorks)
 
 
   return (
@@ -63,7 +75,7 @@ const BookDetailsPage = () => {
         </div>
         
         <div className="info">
-          <h5 className="h5 author"><b>Author: </b><Link to="#">{author}</Link></h5>
+          <h5 className="h5 author"><b>Author: </b><Link to="#">{authorName}</Link></h5>
           <h5 className="h5"><b>Rating: </b>{rating ?? '...' }</h5>
           <h5 className="h5"><b>Editions: </b>{edition_count}</h5>
           <h5 className="h5"><b>First Published:</b> {first_publish_year}</h5>
